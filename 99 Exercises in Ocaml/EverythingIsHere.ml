20635a43b9feef4791cdc93e63025b05b795ecfa
Demetre <shonka.demeloper@gmail.com>
2022-06-18 16:51:51 +0400

    2

--- 99 Exercises in Ocaml/EverythingIsHere.ml
+++ 99 Exercises in Ocaml/EverythingIsHere.ml
@@ -2,4 +2,10 @@ let rec last = function
   | [] -> None
   | [ x ] -> Some x
   | _ :: t -> last t;;
-  
+
+
+
+  let rec last_two = function
+  | [] | [_] -> None
+  | [x; y] -> Some (x,y)
+  | _ :: t -> last_two t;;
