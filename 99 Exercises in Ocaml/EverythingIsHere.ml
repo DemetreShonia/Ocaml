2b9e8733ce49eb2d150c8f4a7335f9d21fc1b74b
Demetre <shonka.demeloper@gmail.com>
2022-06-19 12:25:56 +0400

    12

--- 99 Exercises in Ocaml/EverythingIsHere.ml
+++ 99 Exercises in Ocaml/EverythingIsHere.ml
@@ -100,5 +100,40 @@ let rec compress = function
         else aux 0 ((create_tuple (count + 1) a) :: acc) t
         in rev (aux 0 [] lst);;
 
-        encode_type ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+        (* encode_type ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
       
+        let decode list =
+          let rec many acc n x =
+            if n = 0 then acc else many (x :: acc) (n - 1) x
+          in
+          let rec aux acc = function
+            | [] -> acc
+            | One x :: t -> aux (x :: acc) t
+            | Many (n, x) :: t -> aux (many acc n x) t
+          in
+            aux [] (List.rev list);;
+        
+          decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];;
+        (* - : string list = *)
+        (* ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] *)
+
+      let decode lst =
+        let rec many count acc x =
+          if count = 0 then acc else many (count - 1) (x::acc) x
+          in
+          let rec aux acc = function
+          | [] -> acc
+          | One x :: t -> aux (x::acc) t
+          | Many (n, x) :: t -> aux (many n acc x) t in
+          aux [] (rev lst);;
+
+
+      let encode list =
+        let rle count x = if count = 0 then One x else Many (count + 1, x) in
+        let rec aux count acc = function
+          | [] -> [] (* Can only be reached if original list is empty *)
+          | [x] -> rle count x :: acc
+          | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                                  else aux 0 (rle count a :: acc) t
+        in
+          List.rev (aux 0 [] list);;
