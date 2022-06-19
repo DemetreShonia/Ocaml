98d7d27ca225ccc54fa01f20c34b948d190312aa
Demetre <shonka.demeloper@gmail.com>
2022-06-19 11:17:03 +0400

    11

--- 99 Exercises in Ocaml/EverythingIsHere.ml
+++ 99 Exercises in Ocaml/EverythingIsHere.ml
@@ -70,15 +70,35 @@ let rec compress = function
      (* ["e"; "e"; "e"; "e"]] *)
 
 
+      let encode lst =
+        let rec aux count acc = function
+        | [] -> []
+        | [x] -> (count + 1, x) :: acc
+        | a::(b::_ as t) -> if a = b then aux (count + 1) acc t
+        else aux 0 ((count + 1, a)::acc) t
+        in aux 0 [] lst;; 
 
-     let encode lst =
-      let rec aux count acc = function
-      | [] -> []
-      | [x] -> (count + 1, x) :: acc
-      | a::(b::_ as t) -> if a = b then aux (count + 1) acc t
-      else aux 0 ((count + 1, a) :: acc) t
-      in aux 0 [] lst;;
 
-      encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+      (* encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
      (* - : (int * string) list = *)
      (* [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] *)
+
+
+     type 'a rle =
+      | One of 'a
+      | Many of int * 'a;;
+    type 'a rle = One of 'a | Many of int * 'a
+    
+    let encode_type lst =
+      let create_tuple count elem =
+        if count = 1 then One elem
+        else Many (count,elem) in
+        let rec aux count acc = function
+        |[] -> []
+        | [x] -> (create_tuple (count + 1) x)::acc
+        | a::(b::_ as t) -> if a = b then aux (count + 1) acc t
+        else aux 0 ((create_tuple (count + 1) a) :: acc) t
+        in rev (aux 0 [] lst);;
+
+        encode_type ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+      
